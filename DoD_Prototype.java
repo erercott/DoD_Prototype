b6110fad7f8b1b99065152374ba194691da53916
Nmodern <noxsweatnoxshirtx@hotmail.com>
2025-11-29 15:03:50 -0800

    Added central gameplay mechanic, a stepwise rotating triangle

---
+++ DoD_Prototype.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.AffineTransform;
+
+public class DoD_Prototype extends JPanel implements KeyListener {
+
+    private int orientation = 0; // 0=right, 1=down, 2=left, 3=up
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        // Define triangle relative to its center
+        int size = 50; // triangle size
+        int[] xPoints = {0, -size, -size};  // tip on the right
+        int[] yPoints = {0, -size, size};   // tip centered vertically
+
+        AffineTransform old = g2d.getTransform();
+        g2d.translate(getWidth() / 2, getHeight() / 2); // center of window
+        g2d.rotate(Math.toRadians(orientation * 90));
+        g2d.fillPolygon(xPoints, yPoints, 3);
+        g2d.setTransform(old);
+    }
+
+    private void rotateRight() {
+        orientation = (orientation + 1) % 4;
+        repaint();
+    }
+
+    private void rotateLeft() {
+        orientation = (orientation + 3) % 4; // equivalent to -1 mod 4
+        repaint();
+    }
+
+    public DoD_Prototype() {
+        setFocusable(true);
+        addKeyListener(this);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_RIGHT:
+                rotateRight();
+                break;
+            case KeyEvent.VK_LEFT:
+                rotateLeft();
+                break;
+        }
+    }
+
+    @Override public void keyReleased(KeyEvent e) {}
+    @Override public void keyTyped(KeyEvent e) {}
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("DoD Prototype");
+        DoD_Prototype panel = new DoD_Prototype();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 600);
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+}
