1fd07af9d61ee15a09e67dabe018584697c1857b
Nmodern <noxsweatxnoxshirtx@hotmail.com>
2025-12-15 13:52:20 -0800

    Add intro and menu; start gameplay on DOWN

--- DoD_Prototype.java
+++ DoD_Prototype.java
@@ -1,22 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import javax.imageio.ImageIO;
+import java.awt.geom.AffineTransform;
 
 public class DoD_Prototype extends JPanel implements KeyListener {
 
+    // --- Intro ---
     private ArrayList<StoryFrame> introFrames = new ArrayList<>();
     private boolean inIntro = true;
     private int currentFrame = 0;
     private int lettersShown = 0;
     private long frameCompleteTime = 0;
-    private final int lingerMillis = 7000; // 7 seconds after text finished
+    private final int lingerMillis = 7000;
+
+    // --- Menu ---
+    private boolean inMenu = false;
+    private int menuOrientation = 0;
+    private Color menuColor = Color.RED;
+
+    // --- Gameplay ---
+    private boolean gameStarted = false;
+    private int strobeIndex = 0;
 
     public DoD_Prototype() {
         setFocusable(true);
@@ -37,12 +47,10 @@ public class DoD_Prototype extends JPanel implements KeyListener {
             ));
         } catch (IOException e) { e.printStackTrace(); }
 
-        // Typewriter + linger timer
         new Timer(50, e -> {
             if (inIntro && currentFrame < introFrames.size()) {
                 StoryFrame frame = introFrames.get(currentFrame);
                 int totalLetters = Arrays.stream(frame.lines).mapToInt(String::length).sum();
-
                 if (lettersShown < totalLetters) {
                     lettersShown++;
                 } else {
@@ -51,15 +59,20 @@ public class DoD_Prototype extends JPanel implements KeyListener {
                         lettersShown = 0;
                         currentFrame++;
                         frameCompleteTime = 0;
-                        if (currentFrame >= introFrames.size()) inIntro = false;
+                        if (currentFrame >= introFrames.size()) {
+                            inIntro = false;
+                            inMenu = true;
+                        }
                     }
                 }
                 repaint();
             }
         }).start();
 
-        // General repaint
-        new Timer(16, e -> repaint()).start();
+        new Timer(16, e -> {
+            if (gameStarted) strobeIndex++;
+            repaint();
+        }).start();
     }
 
     @Override
@@ -67,15 +80,13 @@ public class DoD_Prototype extends JPanel implements KeyListener {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
 
-        // Black background
         g2d.setColor(Color.BLACK);
         g2d.fillRect(0, 0, getWidth(), getHeight());
 
-        // Intro sequence
+        // --- Intro sequence ---
         if (inIntro && currentFrame < introFrames.size()) {
             StoryFrame frame = introFrames.get(currentFrame);
 
-            // Scale image to half panel size
             int imgW = frame.image.getWidth();
             int imgH = frame.image.getHeight();
             int maxW = getWidth() / 2;
@@ -86,11 +97,8 @@ public class DoD_Prototype extends JPanel implements KeyListener {
             int imgX = (getWidth() - drawW) / 2;
             int imgY = 50;
 
-            g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-                                 RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
             g2d.drawImage(frame.image, imgX, imgY, drawW, drawH, null);
 
-            // Draw centered typewriter text at bottom
             g2d.setColor(Color.WHITE);
             g2d.setFont(new Font("Times New Roman", Font.BOLD, 24));
             int lineHeight = 30;
@@ -107,18 +115,69 @@ public class DoD_Prototype extends JPanel implements KeyListener {
                 lettersRemaining -= len;
                 if (lettersRemaining <= 0) break;
             }
-            return; // skip gameplay while intro
+            return;
         }
 
-        // Gameplay placeholder
-        g2d.setColor(Color.GRAY);
-        g2d.setFont(new Font("Arial", Font.BOLD, 36));
-        String msg = "Gameplay will appear here...";
-        int msgW = g2d.getFontMetrics().stringWidth(msg);
-        g2d.drawString(msg, (getWidth() - msgW) / 2, getHeight() / 2);
+        // --- Menu ---
+        if (inMenu) {
+            int triSize = Math.min(getWidth(), getHeight()) / 3;
+            int centerX = getWidth()/2;
+            int centerY = getHeight()/2;
+
+            int[] xPoints = {0, -triSize, -triSize};
+            int[] yPoints = {0, -triSize, triSize};
+
+            AffineTransform old = g2d.getTransform();
+            g2d.translate(centerX, centerY);
+            g2d.rotate(Math.toRadians(menuOrientation * 90));
+            g2d.setColor(menuColor);
+            g2d.fillPolygon(xPoints, yPoints, 3);
+            g2d.setTransform(old);
+            return;
+        }
+
+        // --- Gameplay ---
+        if (gameStarted) {
+            float hue = (strobeIndex % 360) / 360f;
+            g2d.setColor(Color.getHSBColor(hue, 1f, 1f));
+            g2d.setStroke(new BasicStroke(5));
+            g2d.drawRect(0, 0, getWidth(), getHeight());
+
+            g2d.setColor(Color.GRAY);
+            g2d.setFont(new Font("Arial", Font.BOLD, 36));
+            String msg = "Gameplay will appear here...";
+            int msgW = g2d.getFontMetrics().stringWidth(msg);
+            g2d.drawString(msg, (getWidth() - msgW) / 2, getHeight() / 2);
+        }
     }
 
-    @Override public void keyPressed(KeyEvent e) {}
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (inIntro && e.getKeyCode() == KeyEvent.VK_SPACE) {
+            inIntro = false;
+            inMenu = true;
+            repaint();
+            return;
+        }
+
+        if (inMenu) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_RIGHT -> menuOrientation = (menuOrientation + 1) % 4;
+                case KeyEvent.VK_LEFT -> menuOrientation = (menuOrientation + 3) % 4;
+                case KeyEvent.VK_UP -> {
+                    if (menuColor.equals(Color.RED)) menuColor = Color.GREEN;
+                    else if (menuColor.equals(Color.GREEN)) menuColor = Color.BLUE;
+                    else menuColor = Color.RED;
+                }
+                case KeyEvent.VK_DOWN -> {
+                    inMenu = false;
+                    gameStarted = true;
+                    repaint();
+                }
+            }
+        }
+    }
+
     @Override public void keyReleased(KeyEvent e) {}
     @Override public void keyTyped(KeyEvent e) {}
 
