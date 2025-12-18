c1733d4afb4a95562862527adcca198db725480f
Nmodern <noxsweatxnoxshirtx@hotmail.com>
2025-12-18 14:11:49 -0800

    Boss Fix

--- DoD_Prototype.java
+++ DoD_Prototype.java
@@ -38,11 +38,17 @@ public class DoD_Prototype extends JPanel implements KeyListener {
     private int level = 0;
     private ArrayList<EnemyOrb> enemyOrbs = new ArrayList<>();
 
+    // --- Final Boss ---
+    private BufferedImage finalBossImage;
+    private boolean inFinalLevel = false;
+    private ArrayList<EnemyOrb> finalOrbs = new ArrayList<>();
+
     public DoD_Prototype() {
         setFocusable(true);
         addKeyListener(this);
 
         try {
+            // Intro frames
             introFrames.add(new StoryFrame(ImageIO.read(new File("scene00.png")),
                     "Once upon a time, there lived a Kingdom inside of a computer...",
                     "One day the computer was infected by an unknown virus."
@@ -55,11 +61,15 @@ public class DoD_Prototype extends JPanel implements KeyListener {
             introFrames.add(new StoryFrame(ImageIO.read(new File("scene02.png")),
                     "This is the story of Random Access Memory"
             ));
+
+            // Final boss image
+            finalBossImage = ImageIO.read(new File("finalBoss.png"));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // Intro typewriter
+        // Intro typewriter effect
         new Timer(50, e -> {
             if (inIntro && currentFrame < introFrames.size()) {
                 StoryFrame frame = introFrames.get(currentFrame);
@@ -91,46 +101,50 @@ public class DoD_Prototype extends JPanel implements KeyListener {
 
     private void spawnLevel(int lvl) {
         enemyOrbs.clear();
-		
-		int numOrbs;
-	
-		switch(lvl) { 
-			case 0 -> { //red
-				numOrbs = 1;
-				
-			}
-			case 1 -> { //blue
-				numOrbs = 3;
-			}
-			case 2 -> {//green
-				numOrbs = 4;
-			}
-			default -> {
-				numOrbs = 3;
-			}
-		}
-		
-		int arenaW = getWidth() - 200;
-		int arenaX = (getWidth() - arenaW) / 2;
-		int centerY = (getHeight() - arenaHeight) / 2 + arenaHeight / 2;
-		
-		int spacing = arenaW / (numOrbs + 1);
-		
-		for (int i = 0; i < numOrbs; i++) {
-			int x = arenaX + spacing * (i + 1) -25;
-			int y = centerY - 25;
-			Color color = switch (lvl % 3){
-				case 0 -> Color.RED;
-				case 1 -> Color.BLUE;
-				default -> Color.GREEN;
-			};
-			int orientation;
-			if (lvl == 0){
-				orientation = 0;
-			} else {
-				orientation = i % 4;
-			}
-			enemyOrbs.add(new EnemyOrb(x,y,50,color,orientation));
+        int numOrbs;
+
+        switch (lvl) {
+            case 0 -> numOrbs = 1;
+            case 1 -> numOrbs = 3;
+            case 2 -> numOrbs = 4;
+            default -> numOrbs = 3;
+        }
+
+        int arenaW = getWidth() - 200;
+        int arenaX = (getWidth() - arenaW) / 2;
+        int centerY = (getHeight() - arenaHeight) / 2 + arenaHeight / 2;
+
+        int spacing = arenaW / (numOrbs + 1);
+
+        for (int i = 0; i < numOrbs; i++) {
+            int x = arenaX + spacing * (i + 1) - 25;
+            int y = centerY - 25;
+            Color color = switch (lvl % 3) {
+                case 0 -> Color.RED;
+                case 1 -> Color.BLUE;
+                default -> Color.GREEN;
+            };
+            int orientation = (lvl == 0) ? 0 : i % 4;
+            enemyOrbs.add(new EnemyOrb(x, y, 50, color, orientation));
+        }
+    }
+
+    private void spawnFinalLevel() {
+        finalOrbs.clear();
+        if (finalBossImage == null) return;
+
+        int orbSize = 50;
+        int[][] eyeCoords = {
+                {217, 165}, {213, 271}, {340, 206}, {473, 103},
+                {567, 231}, {645, 122}, {722, 298}
+        };
+
+        for (int[] coord : eyeCoords) {
+            int orbX = coord[0] - orbSize / 2;
+            int orbY = coord[1] - orbSize / 2;
+            Color orbColor = Color.RED;
+            int orientation = 0;
+            finalOrbs.add(new EnemyOrb(orbX, orbY, orbSize, orbColor, orientation));
         }
     }
 
@@ -150,8 +164,8 @@ public class DoD_Prototype extends JPanel implements KeyListener {
             int maxW = getWidth() / 2;
             int maxH = getHeight() / 2;
             double scale = Math.min((double) maxW / imgW, (double) maxH / imgH);
-            int drawW = (int)(imgW * scale);
-            int drawH = (int)(imgH * scale);
+            int drawW = (int) (imgW * scale);
+            int drawH = (int) (imgH * scale);
             int imgX = (getWidth() - drawW) / 2;
             int imgY = 50;
             g2d.drawImage(frame.image, imgX, imgY, drawW, drawH, null);
@@ -191,13 +205,14 @@ public class DoD_Prototype extends JPanel implements KeyListener {
             return;
         }
 
-        // --- Gameplay ---
+        // --- Gameplay & Final Boss ---
         if (gameStarted) {
+
+            // Strobing arena border
             int arenaW = getWidth() - 200;
             int arenaH = arenaHeight;
             int arenaX = (getWidth() - arenaW) / 2;
             int arenaY = (getHeight() - arenaH) / 2;
-
             float hue = (strobeIndex % 360) / 360f;
             g2d.setColor(Color.getHSBColor(hue, 1f, 1f));
             g2d.setStroke(new BasicStroke(5));
@@ -217,17 +232,39 @@ public class DoD_Prototype extends JPanel implements KeyListener {
             g2d.rotate(Math.toRadians(orientation * 90));
             g2d.setColor(playerColor);
             g2d.fillPolygon(
-                new int[]{0, -triSize, -triSize},
-                new int[]{0, -triSize, triSize},
-                3
+                    new int[]{0, -triSize, -triSize},
+                    new int[]{0, -triSize, triSize},
+                    3
             );
             g2d.setTransform(old);
 
-            // Draw enemies
-            for (int i = 0; i < enemyOrbs.size(); i++){
-				EnemyOrb orb = enemyOrbs.get(i);
-				boolean isActive = (i == 0);
-                orb.draw(g2d, isActive);
+            if (inFinalLevel && finalBossImage != null) {
+                // Draw boss
+                int imgW = finalBossImage.getWidth();
+                int imgH = finalBossImage.getHeight();
+                double scale = Math.min((double) getWidth() / 2 / imgW, (double) getHeight() / 2 / imgH);
+                int drawW = (int) (imgW * scale);
+                int drawH = (int) (imgH * scale);
+                int imgX = (getWidth() - drawW) / 2;
+                int imgY = (getHeight() - drawH) / 2;
+                g2d.drawImage(finalBossImage, imgX, imgY, drawW, drawH, null);
+
+                // Draw boss orbs
+                int[][] eyeCoords = {{217, 165}, {213, 271}, {340, 206}, {473, 103}, {567, 231}, {645, 122}, {722, 298}};
+                for (int i = 0; i < finalOrbs.size(); i++) {
+                    EnemyOrb orb = finalOrbs.get(i);
+                    int screenX = imgX + (int) (eyeCoords[i][0] * ((double) drawW / imgW)) - orb.size / 2;
+                    int screenY = imgY + (int) (eyeCoords[i][1] * ((double) drawH / imgH)) - orb.size / 2;
+                    orb.draw(g2d, true, screenX, screenY, orb.size);
+                }
+
+            } else {
+                // Draw enemies
+                for (int i = 0; i < enemyOrbs.size(); i++) {
+                    EnemyOrb orb = enemyOrbs.get(i);
+                    boolean isActive = (i == 0);
+                    orb.draw(g2d, isActive);
+                }
             }
         }
     }
@@ -239,36 +276,49 @@ public class DoD_Prototype extends JPanel implements KeyListener {
             inMenu = true;
             repaint();
             return;
-        } else if (gameStarted) {
+        }
+
+        if (gameStarted) {
             switch (e.getKeyCode()) {
                 case KeyEvent.VK_RIGHT -> orientation = (orientation + 1) % 4;
                 case KeyEvent.VK_LEFT -> orientation = (orientation + 3) % 4;
-                /*case KeyEvent.VK_UP -> {
-                    if (playerColor.equals(Color.RED)) playerColor = Color.GREEN;
-                    else if (playerColor.equals(Color.GREEN)) playerColor = Color.BLUE;
-                    else playerColor = Color.RED;
-                } */
                 case KeyEvent.VK_DOWN -> {
-                    if (!enemyOrbs.isEmpty()) {
-                        EnemyOrb first = enemyOrbs.get(0);
+                    ArrayList<EnemyOrb> activeOrbs = inFinalLevel ? finalOrbs : enemyOrbs;
+                    if (!activeOrbs.isEmpty()) {
+                        EnemyOrb first = activeOrbs.get(0);
                         if (first.matchesPlayer(orientation, playerColor)) {
-                            enemyOrbs.remove(0);
-
-                            if (enemyOrbs.isEmpty()) {
-                                // Unlock next color in sequence
-                                if (playerColor.equals(Color.RED)) playerColor = Color.BLUE;
-                                else if (playerColor.equals(Color.BLUE)) playerColor = Color.GREEN;
-                                else if (playerColor.equals(Color.GREEN)) playerColor = Color.RED;
-
-                                level++;
-                                spawnLevel(level);
+                            activeOrbs.remove(0);
+                            if (activeOrbs.isEmpty()) {
+                                if (!inFinalLevel) {
+                                    // Cycle color per level
+                                    if (playerColor.equals(Color.RED)) playerColor = Color.BLUE;
+                                    else if (playerColor.equals(Color.BLUE)) playerColor = Color.GREEN;
+                                    else if (playerColor.equals(Color.GREEN)) {
+                                        inFinalLevel = true;
+                                        spawnFinalLevel();
+                                    }
+                                    level++;
+                                    spawnLevel(level);
+                                } else {
+                                    System.out.println("Final boss defeated!");
+                                }
                             }
                         }
                     }
                 }
+                case KeyEvent.VK_UP -> {
+                    if (inFinalLevel) {
+                        // Player color cycling manually during final boss
+                        if (playerColor.equals(Color.RED)) playerColor = Color.GREEN;
+                        else if (playerColor.equals(Color.GREEN)) playerColor = Color.BLUE;
+                        else if (playerColor.equals(Color.BLUE)) playerColor = Color.RED;
+                    }
+                }
             }
             repaint();
-        } else if (inMenu) {
+        }
+
+        if (inMenu) {
             switch (e.getKeyCode()) {
                 case KeyEvent.VK_RIGHT -> menuOrientation = (menuOrientation + 1) % 4;
                 case KeyEvent.VK_LEFT -> menuOrientation = (menuOrientation + 3) % 4;
@@ -287,8 +337,10 @@ public class DoD_Prototype extends JPanel implements KeyListener {
         }
     }
 
-    @Override public void keyReleased(KeyEvent e) {}
-    @Override public void keyTyped(KeyEvent e) {}
+    @Override
+    public void keyReleased(KeyEvent e) {}
+    @Override
+    public void keyTyped(KeyEvent e) {}
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("DoD Prototype");
--- EnemyOrb.java
+++ EnemyOrb.java
@@ -12,40 +12,46 @@ public class EnemyOrb {
         this.size = size;
         this.triangleColor = triangleColor;
         this.orientation = orientation;
-		
-		}
-		
-	public boolean matchesPlayer(int playerOrientation, Color playerColor) {
-		return this.orientation == playerOrientation && this.triangleColor.equals(playerColor);
     }
 
+    // Check if the orb matches the player orientation and color
+    public boolean matchesPlayer(int playerOrientation, Color playerColor) {
+        return this.orientation == playerOrientation && this.triangleColor.equals(playerColor);
+    }
+
+    // --- Original draw method for normal gameplay ---
     public void draw(Graphics2D g2d, boolean active) {
-        // Outline circle
-		int cx = x + size / 2;
-		int cy = y + size / 2;
-		int s = size / 3;
-		
+        draw(g2d, active, this.x, this.y, this.size);
+    }
+
+    // --- New draw method for final boss with scaled coordinates ---
+    public void draw(Graphics2D g2d, boolean active, int drawX, int drawY, int drawSize) {
+        int cx = drawX + drawSize / 2;
+        int cy = drawY + drawSize / 2;
+        int s = drawSize / 3;
+
         int[] xPoints = {0, -s, -s};
         int[] yPoints = {0, -s, s};
-		
-		AffineTransform old = g2d.getTransform();
-		
-		g2d.translate(cx, cy);
-		g2d.rotate(Math.toRadians(orientation * 90));
-		
-		g2d.setColor(triangleColor);
-		g2d.fillPolygon(xPoints, yPoints, 3);
-		
-		g2d.setTransform(old);
-		
-		if (active) {
-			g2d.setColor(triangleColor);
-			g2d.setStroke(new BasicStroke(2));
-			g2d.drawOval(x - 6, y - 6, size + 12, size + 12);
-		}
-    // Draw orb outline
-    g2d.setColor(Color.BLACK);
-    g2d.setStroke(new BasicStroke(3));
-    g2d.drawOval(x, y, size, size);
-	}
+
+        AffineTransform old = g2d.getTransform();
+
+        // Draw triangle rotated based on orientation
+        g2d.translate(cx, cy);
+        g2d.rotate(Math.toRadians(orientation * 90));
+        g2d.setColor(triangleColor);
+        g2d.fillPolygon(xPoints, yPoints, 3);
+        g2d.setTransform(old);
+
+        // Draw active outline if orb is the “current target”
+        if (active) {
+            g2d.setColor(triangleColor);
+            g2d.setStroke(new BasicStroke(2));
+            g2d.drawOval(drawX - 6, drawY - 6, drawSize + 12, drawSize + 12);
+        }
+
+        // Draw orb outline
+        g2d.setColor(Color.BLACK);
+        g2d.setStroke(new BasicStroke(3));
+        g2d.drawOval(drawX, drawY, drawSize, drawSize);
+    }
 }
